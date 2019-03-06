fd65540d7f674c1a5cf1a0e38b2f0a729a409952
nurizzzka <nuriza.akynbaeva@mail.ru>
2019-03-06 21:27:06 +0600

    for

--- todo/views.py
+++ todo/views.py
@@ -3,5 +3,8 @@ from .models import *
 
 
 def index(request):
-	context = {}
+	todo_list = TodoList.objects.all()
+
+	context = {'todo_list' : todo_list}
+	
 	return render(request, 'todo/index.html', context)
