08ab863be8550ff52bb3f0dd13e536a715210ed4
Nuriza Akynbaeva <39913575+nurizzzka@users.noreply.github.com>
2019-04-23 17:09:42 +0600

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'todo',
+    'diary_mood',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', include('todo.urls')),
+    path('mood/', include('diary_mood.urls')),
 ]
